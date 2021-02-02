d1506d348d927a49c935bfc1fdaf95e2975a249b
javim7 <61723252+javim7@users.noreply.github.com>
2021-02-02 17:21:30 -0600

    Leer archivo texto
    haciendo que la clase logre leer el archivo de texto y jalar la operacion.

--- Calculadora.java
+++ Calculadora.java
@@ -1,3 +1,35 @@
+//importando clases externas
+import java.io.File;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+
 public class Calculadora {
+
     
-}
+    /** 
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        //se utiliza un try catch para aseguranos que el archivo pueda ser leido correctamente 
+        try{
+
+            File archivoTexto = new File("datos.txt"); //creando nuestro nuevo archivo
+
+            Scanner scan = new Scanner(archivoTexto); //instanciando la clase scanner con el archivo
+
+            String postfixobtenido = scan.nextLine(); //guardando el postfix como una variable
+
+            System.out.println("\nPostfix obtenido: " + postfixobtenido + "\n"); //imprimiendo el postfix
+
+            System.out.println("ENTRADA          OPERACION                                       PILA");
+
+            //pasando el postfix al metodo Calcu 
+            Calcu(postfixobtenido);
+
+        } catch (FileNotFoundException errorArchivoNoEncontrado) {
+            // Se le advierte al usuario que el archivo no es existente, se termina el programa.
+            System.out.println("El archivo de texto datos.txt no ha sido encontrado.");
+        }
+
+    }
