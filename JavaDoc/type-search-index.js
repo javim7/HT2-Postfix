a4e6afa2d895254259bdea06cdea346cc6625f94
javim7 <61723252+javim7@users.noreply.github.com>
2021-02-02 20:10:55 -0600

    javadoc

---
+++ JavaDoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Calculadora"},{"p":"<Unnamed>","l":"CalculadoraInt"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Stack"},{"p":"<Unnamed>","l":"StackVector"}]
